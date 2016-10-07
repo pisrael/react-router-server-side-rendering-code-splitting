31621162bdb944e28a74191b8342c733e4b45526
pedro.israel@gmail.com <pedroisrael@Pedros-MacBook-Pro.local>
2016-10-07 11:13:21 -0300

    Clean up

--- modules/routes/RootRoute.js
+++ modules/routes/RootRoute.js
@@ -1,6 +1,3 @@
-// polyfill webpack require.ensure
-if (typeof require.ensure !== 'function') require.ensure = (d, c) => c(require)
-
 import App from '../components/App'
 import Index from '../components/Index'
 
