6f7972f4f2c0dff8b59538c1cdb1489c56819fef
pedro.israel@gmail.com <pedroisrael@Pedros-MacBook-Pro.local>
2016-10-11 07:56:38 -0300

    lazy load per component

--- modules/routes/LazyLoadRoute.js
+++ modules/routes/LazyLoadRoute.js
@@ -1,7 +1,10 @@
-import LazyLoad from '../components/LazyLoad'
 
 export default {
   path: 'lazyload',
-  component: LazyLoad
+  getComponents(nextState, cb) {
+    require.ensure([], (require) => {
+      cb(null, [require('../components/LazyLoad').default])
+    })
+  }
 }
 
--- modules/routes/RootRoute.js
+++ modules/routes/RootRoute.js
@@ -4,11 +4,9 @@ import Index from '../components/Index'
 export default {
   path: '/',
   component: App,
-  getChildRoutes(location, cb) {
-    require.ensure([], (require) => {
-      cb(null, [require('./LazyLoadRoute').default])
-    })
-  },
+  childRoutes: [
+    require('./LazyLoadRoute').default
+  ],
   indexRoute: {
     component: Index
   }
